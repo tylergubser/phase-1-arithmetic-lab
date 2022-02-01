28af617c59aafdc69c8bd9cfd6d6ce4977fcb78f
tylergubser <tylergubser2200@gmail.com>
2022-02-01 09:13:35 -0600

    commit tree d132116e6008428f466b932f8cbcbaeab4f09732

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
+
+const num3 = 31;
+const num4 = 9;
+const mod = num3 % num4;
+
+
+const random = Math.floor(Math.random()* 10) + 1;
+
+const max = Math.max(15, 16, 17, 18, 19, 20);
+
+//Number.parseInt(random, 10);
 // Write your code here
